34610ddc1ad16ad9cd5c40b38cce785edd5986cd
sy-permana <suryapermana996@gmail.com>
2020-10-14 23:36:19 +0800

    Deploy - change apolloclient uri

--- client/src/config/graphql.js
+++ client/src/config/graphql.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 import { GET_FAVORITE } from './query/graphqlQuery'
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/",
+  uri: "http://18.141.57.26:5000/",
   cache: new InMemoryCache()
 })
 
